05b97045e02c630a394e8778ca788a1529ae829f
danrigg11 <dan_rigg11@hotmail.com>
2018-04-02 14:54:17 +0100

    Change server

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'portfolio_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'Missnash41',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
